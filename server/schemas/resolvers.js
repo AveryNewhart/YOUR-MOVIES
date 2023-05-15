8f6dff09f4a1bbbee605e5875ac01016e7053879
AveryNewhart <newhartreeceavery@gmail.com>
2023-05-14 22:13:20 -0400

    one more step closer

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -141,13 +141,25 @@ const resolvers = {
     },
 
     createReview: async (parent, { review }, context) => {
-      if (context.user) {
-        return (updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { reviews: review } },
-          { new: true, runValidators: true }
-        ));
-      }
+      // if (context.user) {
+      //   return (updatedUser = await User.findOneAndUpdate(
+      //     { _id: context.user._id },
+      //     { $addToSet: { reviews: review } },
+      //     { new: true, runValidators: true }
+      //   ));
+      // }
+    if (context.user) {
+      const reviewWithId = {
+        reviewId: `${Date.now()}-${Math.floor(Math.random() * 1000)}`,
+        ...review,
+      };
+
+    return (updatedUser = await User.findOneAndUpdate(
+      { _id: context.user._id },
+      { $addToSet: { reviews: reviewWithId } },
+      { new: true, runValidators: true }
+    ));
+  }
       throw new AuthenticationError("You need to be logged in!");
     },
 
