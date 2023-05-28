4952fd506f02e3c6b3415076d52ac744027613fc
AveryNewhart <newhartreeceavery@gmail.com>
2023-05-28 15:20:49 -0400

    changing server and connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+require('dotenv').config();
+// let uri = "mongodb+srv://avery:password12345@the-flick-tracker.dnbkejv.mongodb.net/?retryWrites=true&w=majority"
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/yourmovies" //! We may need to change this connection string in the future
+// const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/yourmovies',
+  {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useFindAndModify: false
+
+  // useCreateIndex: true,
+}
 );
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
@@ -33,7 +33,16 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 
+  // db.once("open", () => {
+  //   app.listen(PORT, () => {
+  //     console.log(`API server running on port ${PORT}!`);
+  //     console.log(
+  //       `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+  //     );
+  //   });
+  // });
   db.once("open", () => {
+    const PORT = process.env.PORT || 3001;
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(
