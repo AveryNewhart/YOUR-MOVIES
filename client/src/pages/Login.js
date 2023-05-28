f67b121b5c66fde1a2dd79ea926797458d498e7c
AveryNewhart <newhartreeceavery@gmail.com>
2023-05-28 15:04:58 -0400

    making sure string is correct

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -18,6 +18,7 @@ const LoginForm = () => {
   // const [validated] = useState(false);
   // const [showAlert, setShowAlert] = useState(false);
   
+  // eslint-disable-next-line 
   const [loginUser, { error, data }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
