83937350d5213ba0b532450e99eab14ab7fe6d3d
AveryNewhart <newhartreeceavery@gmail.com>
2023-05-28 16:38:09 -0400

    forgot to npm i

--- server/server.js
+++ server/server.js
@@ -37,14 +37,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 
-  // db.once("open", () => {
-  //   app.listen(PORT, () => {
-  //     console.log(`API server running on port ${PORT}!`);
-  //     console.log(
-  //       `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-  //     );
-  //   });
-  // });
   db.once("open", () => {
     const PORT = process.env.PORT || 3001;
     app.listen(PORT, () => {
